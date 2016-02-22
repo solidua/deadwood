b452e836964861091d4c51bc3f4d11fb8b6e1053
luketerry147 <luketerry147@gmail.com>
2016-02-21 19:59:57 -0800

    Update Player.java
    Should be working properly now

--- Player.java
+++ Player.java
@@ -105,26 +105,34 @@ public class Player implements Dice{
          if(position.getRoomType().equals("Scene")){
             Role takeRole = chooseRole();
             //Find out what rank they want to go to
-         }else if (position.getRoomType().equals("Casting Office")){
+         }else if (position.getRoomType().equals("Casting Office") && rank < 6){
+               Scanner darkly = new Scanner(System.in);
+               boolean upgrading = true;
                System.out.println("You are in the Casting Office, what rank would you like to upgrade to? Enter 0 for no upgrade");
                System.out.print("2 is $4 or 5 credits, 3 is $10 or 10 credits, 4 is $18 or 15 credits, 5 is $28 dollars or 20 credits, 6 is $40 or 25 credits");
-               Scanner darkly = new Scanner(System.in);
-               int wantedRank = 1;
-               boolean okInput = false;
-               try{
-                     while (!okInput){
-                           wantedRank = darkly.nextInt();
-                           if (wantedRank == 0 || (wantedRank >= 2 && wantedRank <= 6)){
-                                 okInput = true;
-                           }else{
-                                 System.out.println("Wrong value given, please enter 0 for no rank upgrade or a value between 2 and 6: ");
-                           }
-                     }
-               }catch (InputMismatchException e){
-                     System.out.println("Wrong form of input, no rank upgrade will be done.");
-               }
-               if (wantedRank != 0){
-                     rank = upgrRank(wantedRank);
+               while(upgrading){
+                  int wantedRank = 1;
+                  int saveRank = 0;
+                  boolean okInput = false;
+                  try{
+                        while (!okInput){
+                              wantedRank = darkly.nextInt();
+                              if (wantedRank == 0 || (wantedRank > rank && wantedRank <= 6)){
+                                    okInput = true;
+                              }else{
+                                    System.out.println("Wrong value given, please enter 0 for no rank upgrade or a value between your current rank, " + rank + ", and 6: ");
+                              }
+                        }
+                  }catch (InputMismatchException e){
+                        System.out.println("Wrong form of input, no rank upgrade will be done.");
+                  }
+                  if (wantedRank != 0){                        
+                        saveRank = upgrRank(wantedRank);                        
+                  }
+                  if(saveRank > 0){
+                     upgrading = false;
+                     rank = saveRank;
+                  }
                }  
          }
       }else{
@@ -138,35 +146,40 @@ public class Player implements Dice{
       Role[] goodRoles = printRoles(availableRoles);
       System.out.println("Choose your role by typing in its name, or quit to not take a role.");
       boolean inputGood = false;
-      int numGoodRoles = goodRoles.length(); 
+      int takeThisRole = -1;
+      Role roleTaken = null;
+      int numGoodRoles = goodRoles.length; 
       while(!inputGood){
          String input = getInput();
          for(int i = 0; i < numGoodRoles; i++){
             if(input.equals(goodRoles[i].getName())){
                inputGood = true;
+               takeThisRole = i;
             }
          }
       }
-      Role roleTaken = goodRoles[i];
-      roleTaken.setTaken(true);
+      if(takeThisRole > 0){
+         roleTaken = goodRoles[takeThisRole];
+         roleTaken.setTaken(true);
+      }
       return roleTaken;
    }
    
    private Role[] printRoles(Role[] allRoles){
       System.out.println("The following roles are available to you:");
-      Role[] goodRoles;
+      Role[] goodRoles = new Role[7];
       int k = 0;
       for(int i = 0; i < 7; i++){
          if(!allRoles[i].getTaken() && (allRoles[i].getLevel() <= rank)){
-            System.out.println("\t", allRoles[i].getName());
+            System.out.println("\t" + allRoles[i].getName());
             goodRoles[k] = allRoles[i];
             k++;
          }
       }
       System.out.println("The following roles are either taken or too high rank for you!");
       for(int i = 0; i < 7; i++){
-         if(allRoles[i].getTaken || (allRoles[i].getLevel() > rank)){
-            System.out.println("\t", allRoles[i].getName());
+         if(allRoles[i].getTaken() || (allRoles[i].getLevel() > rank)){
+            System.out.println("\t" + allRoles[i].getName());
          }
       }
       return goodRoles;
@@ -176,12 +189,12 @@ public class Player implements Dice{
    public int upgrRank(int newrank){
       boolean inputGood = false;
       Scanner scanInput = new Scanner(System.in);      
-      if(this.position.getRoomType().equals("Csating Office")){
+      if(this.position.getRoomType().equals("Casting Office")){
         System.out.println("Do you want to pay for your new rank with money or credit?");
+        System.out.print("2 is $4 or 5 credits, 3 is $10 or 10 credits, 4 is $18 or 15 credits, 5 is $28 dollars or 20 credits, 6 is $40 or 25 credits");
         while(!inputGood){
-
           System.out.println("You currently have " + credits + " credits and " + money + " dollars.");
-          int input = getInput();
+          String input = getInput();
           UtilityScene castingOffice = (UtilityScene)position;
           if(input.equals("money") && (this.money > castingOffice.improveRank(newrank, "money"))){
             setRank(newrank);
@@ -190,6 +203,7 @@ public class Player implements Dice{
             setRank(newrank);
             inputGood = true;
           }else if(input.equals("quit")){
+            setRank(-1);
             inputGood = true;
           }else if(!input.equals("credit") && !input.equals("money")){
             System.out.println("Wrong input! Please input either money or credit for your preferred method of payment!");
